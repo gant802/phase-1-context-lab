13f4e6ba157edca628e91915d9e88af8b4c7b94e
Grant Cummings <grantcummings4429@gmail.com>
2024-04-15 17:39:54 -0400

    finished lab

--- index.js
+++ index.js
@@ -9,6 +9,74 @@
  for you to use if you need it!
  */
 
+ const createEmployeeRecord = ([firstName, familyName, title, payPerHour]) => ({
+    firstName,
+    familyName,
+    title,
+    payPerHour,
+    timeInEvents: [],
+    timeOutEvents: []
+})
+
+const createEmployeeRecords = arrayOfArrays => arrayOfArrays.map(array => createEmployeeRecord(array))
+
+const createTimeInEvent = function (timeInString) {
+    const date = timeInString.substr(0, 10)
+    const time = timeInString.substr(11)
+    const parsedTime = parseInt(time)
+    const timeInEvent = {
+        type: "TimeIn",
+        hour: parsedTime,
+        date: date
+    }
+    this.timeInEvents.push(timeInEvent)
+    return this
+}
+
+const createTimeOutEvent = function (timeOutString) {
+    const date = timeOutString.substr(0, 10)
+    const time = timeOutString.substr(11)
+    const parsedTime = parseInt(time)
+    const timeOutEvent = {
+        type: "TimeOut",
+        hour: parsedTime,
+        date: date
+    }
+    this.timeOutEvents.push(timeOutEvent)
+    return this
+}
+
+const hoursWorkedOnDate = function (dateString) {
+    const foundTimeInEvent = this.timeInEvents.find(timeInObj => {
+        return timeInObj.date === dateString
+    })
+    const foundTimeOutEvent = this.timeOutEvents.find(timeOutObj => {
+        return timeOutObj.date === dateString
+    })
+
+    const hoursWorked = (foundTimeOutEvent.hour - foundTimeInEvent.hour) / 100
+    return hoursWorked
+}
+
+const wagesEarnedOnDate = function (dateString) {
+    const hoursWorked = hoursWorkedOnDate.call(this, dateString)
+    const employeeWage = this.payPerHour
+    return hoursWorked * employeeWage
+}
+
+const findEmployeeByFirstName = function(srcArray, firstName) {
+    const foundEmployee = srcArray.find(employee => employee.firstName === firstName)
+    return foundEmployee ? foundEmployee : undefined
+}
+
+const calculatePayroll = function (arrOfEmployees) {
+    const payroll = arrOfEmployees.reduce((accumulator, employee) => {
+     return accumulator += allWagesFor.call(employee) 
+    }, 0)
+    return payroll
+}
+
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
